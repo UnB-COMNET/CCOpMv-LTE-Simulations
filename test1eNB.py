4edcfa5c44a701f40d4da49f2e22386a8ecfb414
GiordanoSM <giordano@suffert.com>
2021-07-14 16:29:48 -0300

    Version with 1 eNB only

--- test1eNB.py
+++ test1eNB.py
@@ -1,12 +1,15 @@
 import helper as hp
 import random
 import typing as ty
+import numpy as np
 
 def main():
 
   filename = 'teste.ini'
   numUEs = 30
   directions = 2
+  pos_macrocell = (500,500)
+  random.seed(123)
 
   with open(filename, 'wt') as f:
     # General
@@ -24,13 +27,14 @@ def main():
     hp.writeSchedulingOptions(f, sched= ['MAXCI', 'DRR', 'PF', 'ALLOCATOR_BESTFIT'])
     hp.writeSeparation(f, "Mobility")
     hp.writeComment(f, text= "eNodeB")
-    hp.writeIniMobility(f,object_name= 'eNB', iniX= 500, iniY= 500)
+    hp.writeIniMobility(f,object_name= 'eNB', iniX= pos_macrocell[0], iniY= pos_macrocell[1])
     hp.writeConstraint(f, object_name= 'eNB')
     hp.writeComment(f, text= "UEs")
     hp.writeNumUEs(f, numUEs)
     hp.nl(f)
+    pos_hotspot, pos_ues = genUEsPos(numUEs, pos_macrocell)
     hp.writeUesMobilityType(f, type= "StationaryMobility")
-    hp.writeUeMobilityPerso(f, number= numUEs, iniX=[100,300,300], iniY=[200,400,400], iniZ=[0,0,0])
+    hp.writeUeMobilityPerso(f, number= numUEs, iniX= [x for x,y in pos_ues], iniY=[y for x,y in pos_ues], iniZ=np.zeros(len(pos_ues)))
     hp.writeConstraint(f, object_name= 'ue[*]')
     hp.writeSeparation(f, "Apps")
     hp.writeNumApps(f, numUEs= numUEs, directions= directions)
@@ -40,6 +44,7 @@ def main():
     hp.writeAppVoipDL(f, numUEs, n_app= 1)
     hp.writeSeparation(f, "Channel Control")
     hp.writePropagation(f, model= "FreeSpaceModel")
+
 
 
 def defaultGeneral(f):
@@ -58,6 +63,25 @@ def defaultGeneral(f):
   hp.writeSeparation(f, "Resource Blocks")
   f.write('''**.numRbDl = 6\n**.numRbUl = 6
 **.binder.numBands = 6 # this value should be kept equal to the number of RBs\n''')
+
+def genUEsPos(numUEs, pos_macrocell):
+  result = []
+  pos_hotspot = dropObject(pos_macrocell, 425, 105)
+  for n in range(numUEs):
+    if random.random() < 0.6666:
+      result.append(dropObject(pos_hotspot, 70, 0))
+    else:
+      result.append(dropObject(pos_macrocell, 425, 35)) #425
+  return pos_hotspot, result
+
+def dropObject(center: tuple, radius, min_distance):
+  not_done = True
+  while not_done:
+    radius_ue = radius * np.sqrt(random.random())
+    theta_ue = 2 * np.pi * random.random()
+    result = (radius_ue*np.cos(theta_ue) + center[0], radius_ue*np.sin(theta_ue) + center[1])
+    not_done = np.linalg.norm(np.array(result) - np.array(center)) < min_distance #distancia euclidiana
+  return result
 
 if __name__ == "__main__":
   main()
