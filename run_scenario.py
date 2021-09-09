fc85f7734b6fbc822a1c3cdf81720fbbe085c072
GiordanoSM <giordano@suffert.com>
2021-09-09 12:38:53 -0300

    new hetnet_mov

--- run_scenario.py
+++ run_scenario.py
@@ -13,9 +13,8 @@ def main():
   seed = 123
 
   # Scenario only changes with num_ues, center and micro_per_small
-  hetb.hetnet_base(filename, directions, center_x, center_y, num_ues, sites, micro_per_small,
-                   small_per_site, seed, with_stop= True)
-  #hetb.hetnet_mov('Network_CCOpMv/simulations/hetnet_mov.ini', directions, center_x, center_y, num_ues, sites, micro_per_small, small_per_site, seed)
+  #hetb.hetnet_base(filename, directions, center_x, center_y, num_ues, sites, micro_per_small, small_per_site, seed, with_stop= True)
+  hetb.hetnet_mov('Network_CCOpMv/simulations/hetnet_mov.ini', directions, center_x, center_y, num_ues, sites, micro_per_small, small_per_site, seed)
 
 if __name__ == "__main__":
   main()
