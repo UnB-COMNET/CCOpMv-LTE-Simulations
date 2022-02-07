7bd4db05a84cdd4b5f769717c320c81d93a69272
GiordanoSM <giordano@suffert.com>
2022-02-07 15:58:12 -0300

    Reorganized output options

--- Functions/gen_ilp_info.py
+++ Functions/gen_ilp_info.py
@@ -36,14 +36,17 @@ make
 
 
   #Determines what the program will show to the user
-  show = 2
+  get_solution = True
+  show_sinr = False
+  show_full = False
+  show_ues = False
 
   #Initiating scenario
   scen = geo.MapChess(d_height, d_width, d_region, chosen_seed= chosen_seed) #100 setores
 
-  if(show == 0):
+  if show_full:
     #Placing UEs: Full
-    scen.placeUEs(type= "Full", n_ues_macro= 60) #72 macros? -> 4320 ues
+    scen.placeUEs(types= "Full", n_ues_macro= 60) #72 macros? -> 4320 ues
     scen.plotUes()
 
   #Placing UEs
@@ -54,19 +57,20 @@ make
   print("-------------Generating sinr map")
   sinr_map = scen.getSinrMap()
 
-  #Showing sinr in file
-  count = 0
-  count2 = 0
-  with open("sinr.txt", 'w') as f:
-    for enb in sinr_map:
-      count = 0
-      f.write("{}:".format(count2))
-      for snr in enb:
-        f.write("\t{}- {}\n".format(count, snr))
-        count += 1
-      count2 += 1
+  if show_sinr:
+    #Showing sinr in file
+    count = 0
+    count2 = 0
+    with open("sinr.txt", 'w') as f:
+      for enb in sinr_map:
+        count = 0
+        f.write("{}:".format(count2))
+        for snr in enb:
+          f.write("\t{}- {}\n".format(count, snr))
+          count += 1
+        count2 += 1
 
-  if (show == 2):
+  if get_solution:
 
     #Generating default parameters
     max_user_antenna_m = [60 for i in range(scen.n_regions)]
@@ -81,7 +85,7 @@ make
     print("-------------Calculating Solution (this may take a while)")
     ccop_mv_MILP(Max_Space= scen.n_regions, Max_Time= 10, users_t_m= users_t_m, MAX_USER_PER_ANTENNA_m= max_user_antenna_m, antenasmap_m= antennas_map_m, snr_map_mn= sinr_map, MIN_SNR_m= min_snr_m)
 
-  elif (show == 1):
+  elif show_ues:
     #Plotting ues configuration over time
     ues_coords = get_ues_time(scen= scen, xml_filename= xml_filename)
     for t_ues in ues_coords:
