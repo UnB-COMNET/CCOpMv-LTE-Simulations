4f71a9593476e75500a3c12d53e53365703acf63
GiordanoSM <giordano@suffert.com>
2022-03-21 19:01:37 -0300

    Adapted to run fixed and varying sliced cases

--- Functions/run_simulations.py
+++ Functions/run_simulations.py
@@ -19,28 +19,32 @@ def main():
   p_size = 1428#40
   app = "video"
   target_f= 10 #Mbps
-  varying = True
+  varyings = [True, False]
 
   for i in range(len(min_sinrs)):
+    for varying in varyings:
 
-    print("Generating configuration files - Min Snr: {}".format(min_sinrs[i]))
+      print("Generating configuration files - Min Snr: {}".format(min_sinrs[i]))
 
+      ini_path_sliced = dir_path + f'ilp_{"varying" if varying else "fixed"}_sliced.ini'
+      config_name_sliced, enbs_sliced_num = ilpc.ilp_sliced_ini(ini_path_sliced, chosen_seed, size_y= size_y, size_x= size_x, size_sector= size_sector, n_macros= n_macros, repetitions= repetitions,
+                                                                min_sinr= min_sinrs[i], num_bands= num_bands, multi_carriers= multi_carriers, is_micro= is_micro, p_size= p_size, app= app, extra_config_name= "VIDEO",
+                                                                time= 1, target_f= target_f, result_dir= result_dir, varying = varying)
+      
+      #ilpc.ilp_ned(network = "ILPFixedNet", n_enbs= enbs_hando_num, size_x= size_x, size_y= size_y) 
+      ilpc.ilp_ned(network = f"ILP{'Varying' if varying else 'Fixed'}Net", n_enbs= enbs_sliced_num, size_x= size_x, size_y= size_y)  
+
+      print("Running simulations - Min Snr: {}".format(min_sinrs[i]))
+      #run_simulation(ini_path= ini_path, config_name= config_name)
+      run_simulation(ini_path= ini_path_sliced, config_name= config_name_sliced)
+
+    #Handover cases
     #ini_path = dir_path + 'ilp_fixed.ini'
     #config_name, enbs_hando_num = ilpc.ilp_fixed_ini(ini_path, chosen_seed, size_y= size_y, size_x= size_x, size_sector= size_sector, n_macros= n_macros, repetitions= repetitions, antennas_regions= [],
     #                                                 min_sinr= min_sinrs[i], num_bands= num_bands, multi_carriers= multi_carriers, is_micro= is_micro, p_size= p_size, app= app, extra_config_name= "VIDEO",
     #                                                 target_f= target_f, result_dir= result_dir)
-
-    ini_path_sliced = dir_path + f'ilp_{"varying" if varying else "fixed"}_sliced.ini'
-    config_name_sliced, enbs_sliced_num = ilpc.ilp_sliced_ini(ini_path_sliced, chosen_seed, size_y= size_y, size_x= size_x, size_sector= size_sector, n_macros= n_macros, repetitions= repetitions,
-                                                              min_sinr= min_sinrs[i], num_bands= num_bands, multi_carriers= multi_carriers, is_micro= is_micro, p_size= p_size, app= app, extra_config_name= "VIDEO",
-                                                              time= 1, target_f= target_f, result_dir= result_dir, varying = varying)
-    
-    #ilpc.ilp_ned(network = "ILPFixedNet", n_enbs= enbs_hando_num, size_x= size_x, size_y= size_y) 
-    ilpc.ilp_ned(network = f"ILP{'Varying' if varying else 'Fixed'}Net", n_enbs= enbs_sliced_num, size_x= size_x, size_y= size_y)  
-
-    print("Running simulations - Min Snr: {}".format(min_sinrs[i]))
+    #ilpc.ilp_ned(network = "ILPFixedNet", n_enbs= enbs_hando_num, size_x= size_x, size_y= size_y)
     #run_simulation(ini_path= ini_path, config_name= config_name)
-    run_simulation(ini_path= ini_path_sliced, config_name= config_name_sliced)
 
 def run_simulation(ini_path: str, config_name: str):
   
