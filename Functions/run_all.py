632bdd48cd5df50b3433e21180b102882f4782a2
Juliano Balçante <j-bal@hotmail.com>
2022-04-24 00:22:18 -0300

    The functions get_missing_solutions(), get_missing_snapshots(), run_missing_solutions(), run_missing_snapshots() have been added.

--- Functions/run_all.py
+++ Functions/run_all.py
@@ -1,11 +1,12 @@
 from ast import Pass
 from math import ceil
 from os import remove
-from pickletools import read_uint1
 import re
 from tkinter.tix import InputOnly
 from typing import List
 from unittest import result
+
+from numpy import empty
 from gen_ilp_info import run_movement_simulation, gen_ilp_info, gen_file_name
 from multiprocessing import Process, cpu_count
 from _5G_Scenarios.ILP_configs import ilp_sliced_ini, ilp_sliced_ini_per_slice, ilp_ned, gen_solver_result_filename, gen_movement_filename, gen_sliced_config_pattern
@@ -21,12 +22,12 @@ SUCCESS = 'SUCCESS'
 
 def main():
     #General configs
-    chosen_seeds = [321]
+    chosen_seeds = [852,471]#
     size_x = 4000
     size_y = 4000
     size_sector = 400
     n_macros = 1
-    min_sinrs = [5, 10]
+    min_sinrs = [0, 1, 3]
     modes = ['varying', 'fixed'] # varying, fixed or single
     result_dir = "Solutions"
     micro_power = 20 #dBm
@@ -75,6 +76,10 @@ def run_multiple_seeds(chosen_seeds: List[int], size_x: int, size_y: int, size_s
                        extra_dir: List[str] = [], num_slices: int= 10, per_slice: bool= True):
     """This function is used to run multiple 'run_all' functions in diferent processes, one for each value in chosen_seeds."""
     
+    # Generating makefile and compiling OMNeT++ and its frameworks
+    print(f'\nRunning makefile.')
+    run_make()  
+    
     # Evaluating maximum runs
     num_seeds = len(chosen_seeds)
     num_modes = len(modes)
@@ -83,12 +88,17 @@ def run_multiple_seeds(chosen_seeds: List[int], size_x: int, size_y: int, size_s
     num_totalRuns =  num_cases * num_slices * repetitions
     print("Simulating at most {} cases, hence {} runs. There are {} CPU cores available".format(num_cases, num_totalRuns, cpu_count()))
     
+    # Checking for the existence of optimizer solution files and running solver for non-existent ones using parallel computing
+    missing_snapshots = get_missing_snapshots(chosen_seeds, move_config_name)
+    run_missing_snapshots(missing_snapshots, size_x, size_y, size_sector, n_macros, project_dir,sim_dir, move_config_name, num_slices)
+    
+    missing_solutions = get_missing_solutions(chosen_seeds, min_sinrs, modes, extra_dir, micro_power)
+    run_missing_solutions(missing_solutions, size_x, size_y, size_sector, n_macros, result_dir, move_config_name, min_dis, first_antenna_region, min_time,\
+                            micro_power, num_slices)
+    
     num_cases_simultaneously = ceil(cpu_count()/(num_slices*repetitions))
     print('Running {} cases simultaneously.'.format(num_cases_simultaneously))
 
-    # Generating makefile and compiling OMNeT++ and its frameworks
-    print(f'\nRunning makefile.')
-    run_make()    
 
     extra_dir = ['chosen_seed'] + extra_dir
 
@@ -176,7 +186,7 @@ def run_all(chosen_seed: int, size_x: int, size_y: int, size_sector: int, n_macr
               'sim_dir': sim_dir, 'num_bands': num_bands, 'repetitions': repetitions, 'slice_time': slice_time, 'p_size': p_size, 'app': app,
               'target_f': target_f, 'extra_config_name': extra_config_name, 'multi_carriers': multi_carriers, 'is_micro': is_micro, 'cmdenv_config': cmdenv_config,
               'min_time': min_time, 'micro_power': micro_power, 'net_dir': net_dir, 'project_dir': project_dir, 'num_slices': num_slices, 'per_slice': per_slice}
-    print(extra_dir)
+    
     for param in extra_dir:
         kwargs['result_dir'] += '/' + param + f'_{kwargs[param]}'
         kwargs['sim_dir'] += '/' + param + f'_{kwargs[param]}'
@@ -189,7 +199,7 @@ def run_all(chosen_seed: int, size_x: int, size_y: int, size_sector: int, n_macr
     result_dir = kwargs['result_dir']
     sim_dir = kwargs['sim_dir']
     net_dir = kwargs['net_dir']
-    print(project_dir, result_dir,sim_dir,net_dir)
+    #print(project_dir, result_dir,sim_dir,net_dir)
     
     if per_slice:
         with parallel_backend('loky'):
@@ -253,7 +263,7 @@ def process_func(chosen_seed: int, size_x: int, size_y: int, size_sector: int, n
                  micro_power: int= 30, num_slices: int= 10, per_slice: bool = True):
     """This function defines the behaviour of each process, running both the solver and the simulation of a single scenario."""
     print("\nRunning case {} {} dB\n".format(mode,min_sinr))
-    print(project_dir, result_dir,sim_dir,net_dir)
+    #print(project_dir, result_dir,sim_dir,net_dir)
     
     check_mode(mode= mode)
 
@@ -261,6 +271,7 @@ def process_func(chosen_seed: int, size_x: int, size_y: int, size_sector: int, n
     sim_path = project_dir + '/' + sim_dir
     #Verifying if solver is already done
     done = compare_last_line(gen_solver_result_filename(result_dir, mode, min_sinr), '--- Done ---\n')
+    
     if done:
         print(f'Solver {file_name} already computed. (Seed: {chosen_seed})')
     # If not done, do it    
@@ -269,7 +280,7 @@ def process_func(chosen_seed: int, size_x: int, size_y: int, size_sector: int, n
         gen_ilp_info(chosen_seed= chosen_seed, size_x= size_x, size_y= size_y, size_sector= size_sector, n_macros= n_macros,
                     xml_filename= xml_filename, min_sinr= min_sinr, result_dir= result_dir, mode= mode, min_dis= min_dis,
                     first_antenna_region= first_antenna_region, min_time= min_time, micro_power= micro_power, num_slices= num_slices)
-
+    
     #Generating config and network files
     print("Generating configuration files - Min Snr: {} - {} (Seed: {})".format(min_sinr, mode.capitalize(), chosen_seed))
     
@@ -363,5 +374,54 @@ def get_missing_simulations(mode: str, num_bands: List[int], repetitions: int, s
 
     return missing
 
+def get_missing_solutions(chosen_seeds: List[int], min_sinrs: List[int], modes: List[str],\
+                          extra_dir: List[str], micro_power: int) -> List[tuple]:
+    """ This function returns the solver solutions that were not computed yet"""
+    missing = []
+    for chosen_seed in chosen_seeds:
+        result_dir = 'Solutions/' + 'chosen_seed' + '_{}'.format(chosen_seed) + '/' + extra_dir[0] + '_{}'.format(micro_power)
+        
+        for mode in modes:
+            for min_sinr in min_sinrs:
+                # Looking for case: 
+                done = compare_last_line(gen_solver_result_filename(result_dir,mode,min_sinr),'--- Done ---\n')
+                if done is not True:
+                    missing.append((chosen_seed,mode,min_sinr))
+
+    return missing
+
+def get_missing_snapshots(chosen_seeds: List[int], move_config_name: str):
+    missing = []
+    for chosen_seed in chosen_seeds:
+        #move_file = gen_movement_filename(move_config_name, chosen_seed, snapshot= False)
+        xml_filename = gen_movement_filename(move_config_name, chosen_seed, snapshot= True)
+        done = compare_last_line(xml_filename, '<!--Done-->\n')
+        if done is not True:
+            missing.append(chosen_seed)
+    return missing
+        
+def run_missing_solutions(missing_solutions: List[tuple], size_x: int, size_y: int, size_sector: int, n_macros: int,
+                 result_dir: str, move_config_name: str, min_dis: int, first_antenna_region: int, min_time: int,
+                 micro_power: int = 30, num_slices: int= 10):
+    if missing_solutions != []:
+        print("Running solver for {} missing solutions".format(len(missing_solutions)))
+        with parallel_backend('loky'):
+            Parallel(n_jobs=cpu_count())(delayed(gen_ilp_info)(chosen_seed, size_x, size_y, size_sector, n_macros,
+                        gen_movement_filename(move_config_name, chosen_seed, snapshot= True), min_sinr, result_dir, mode, min_dis,
+                        first_antenna_region, min_time, micro_power, num_slices) for chosen_seed, mode, min_sinr in missing_solutions)
+
+    #gen_ilp_info(chosen_seed= chosen_seed, size_x= size_x, size_y= size_y, size_sector= size_sector, n_macros= n_macros,
+    #                xml_filename= xml_filename, min_sinr= min_sinr, result_dir= result_dir, mode= mode, min_dis= min_dis,
+    #                first_antenna_region= first_antenna_region, min_time= min_time, micro_power= micro_power, num_slices= num_slices)
+
+def run_missing_snapshots(missing_snapshots: List[int], size_x: int, size_y: int, size_sector: int, n_macros: int, project_dir: str,\
+                          sim_dir: str, move_config_name: str, num_slices):
+    if missing_snapshots != []:
+        print("Running movement simulation for {} seeds".format(len(missing_snapshots)))
+        with parallel_backend('loky'):
+            Parallel(n_jobs=cpu_count())(delayed(run_movement_simulation)(project_dir + '/' + sim_dir + '/' +
+                    gen_movement_filename(move_config_name, chosen_seed, snapshot= False), chosen_seed, size_x, size_y,
+                    size_sector, n_macros, move_config_name, num_slices, cpu_count()) for chosen_seed in missing_snapshots)
+    
 if __name__ == "__main__": 
     main()
