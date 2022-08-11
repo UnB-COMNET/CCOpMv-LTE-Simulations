af18414c2688d5382b1169efcc6fe40c78d4160a
Juliano Balçante <j-bal@hotmail.com>
2022-08-11 13:50:27 -0300

    Adding mobility type based on LinearMobility, but it has startTime parameter to control when the movent starts

---
+++ Network_CCOpMv/_5G/models/LinearMobilityDelayed.cc
@@ -0,0 +1,58 @@
+//
+// Author: Emin Ilker Cetinbas (niw3_at_yahoo_d0t_com)
+// Copyright (C) 2005 Emin Ilker Cetinbas
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "LinearMobilityDelayed.h"
+
+namespace omnetpp {
+
+LinearMobilityDelayed::LinearMobilityDelayed()
+{
+    speed = 10;
+}
+
+void LinearMobilityDelayed::initialize(int stage)
+{
+    MovingMobilityBase::initialize(stage);
+
+    EV_TRACE << "initializing LinearMobility stage " << stage << endl;
+    if (stage == inet::INITSTAGE_LOCAL) {
+        speed = par("speed");
+        startTime = par("startTime");
+        stationary = (speed == 0);
+        inet::rad heading = inet::deg(par("initialMovementHeading"));
+        inet::rad elevation = inet::deg(par("initialMovementElevation"));
+        inet::Coord direction = inet::Quaternion(inet::EulerAngles(heading, -elevation, inet::rad(0))).rotate(inet::Coord::X_AXIS);
+
+        lastVelocity = direction * speed;
+    }
+}
+
+void LinearMobilityDelayed::move()
+{
+    if(simTime() >= startTime){
+        double elapsedTime = (simTime() - lastUpdate).dbl();
+        lastPosition += lastVelocity * elapsedTime;
+
+        // do something if we reach the wall
+        inet::Coord dummyCoord;
+        handleIfOutside(REFLECT, dummyCoord, lastVelocity);
+    }
+}
+
+}
+
